3c8a3d4118e76bbff7bb13a0b48833f4aeb150ba
michael <michaelglenn88@yahoo.com>
2022-03-20 16:54:19 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(val = "roller-skate", sym = "!") {
+  return "This Saturday, I want to " + val + sym;
+}
+
+function mondayWork(val = "go to the office", sym = ".") {
+  return "This Monday, I will " + val + sym;
+}
+
+function wrapAdjective(symbol) {
+  const msg = "You are ";
+  const varthat = function (tex) {
+    // console.log();
+    const warn = "!";
+    return msg + symbol + tex + symbol + warn;
+  };
+  return varthat;
+}
